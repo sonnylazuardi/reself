7222a4166098ce182ecc3b046c8945f4eb5ea6e2
Sonny Lazuardi <sonnylazuardi@gmail.com>
2014-07-16 20:22:01 +0700

    menu frame

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
+var reselfApp = angular.module('reselfApp', ['ngRoute']);
+reselfApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'view/home.html',
+        controller: 'HomeCtrl',
+    })
+    .when('/camera', {
+        templateUrl: 'view/camera.html',
+        controller: 'CameraCtrl',
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+}]);
+
+reselfApp.controller('HomeCtrl', function($scope) {
+
+});
+
+reselfApp.controller('CameraCtrl', function($scope) {
+
+});
 
 window.onload = function () {
-    // TODO:: Do your initialization job
-
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
         if(e.keyName == "back")
             tizen.application.getCurrentApplication().exit();
     });
 
-    // Sample code
-    var textbox = document.querySelector('.contents');
-    textbox.addEventListener("click", function(){
-    	box = document.querySelector('#textbox');
-    	box.innerHTML = box.innerHTML == "Basic" ? "Sample" : "Basic";
-    });
-    
+    setTimeout(function() {
+        $('.logo').fadeOut('slow');
+    }, 1500);
 };
+
