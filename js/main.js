4c2f591f67368c621371c2462b4573be5c6c1ddd
Sonny Lazuardi <sonnylazuardi@gmail.com>
2014-07-17 02:02:37 +0700

    setting finished

--- js/main.js
+++ js/main.js
@@ -22,17 +22,53 @@ reselfApp.config(['$routeProvider', function ($routeProvider) {
     });
 }]);
 
+reselfApp.factory('Setting', function() {
+    var setting = {
+        delay : 10,
+        burst : 5, 
+        flash : 0
+    }
+    return setting;
+});
+
+
 reselfApp.controller('HomeCtrl', function($scope) {
     $scope.exit = function() {
         tizen.application.getCurrentApplication().exit();
     }
 });
 
-reselfApp.controller('SettingCtrl', function($scope) {
-    $scope.delay = 10;
-    $scope.burst = 0;
-    $scope.flash = true;
+reselfApp.controller('SettingCtrl', function($scope, Setting) {
+    $scope.delay = Setting.delay;
+    $scope.delays = [
+        10, 5, 2
+    ];
+    $scope.burst = Setting.burst;
+    $scope.bursts = [
+        0, 2, 5
+    ];
+    $scope.flash = Setting.flash;
+    $scope.flashes = [
+        {
+            id: 0,
+            name: 'Off'
+        },
+        {
+            id: 1,
+            name: 'On'
+        }
+    ];
+    $scope.$watch('delay', function() {
+        Setting.delay = $scope.delay;
+    });
+    $scope.$watch('burst', function() {
+        Setting.burst = $scope.burst;
+    });
+    $scope.$watch('flash', function() {
+        Setting.flash = $scope.flash;
+    });
 });
+
 
 reselfApp.controller('CameraCtrl', function($scope) {
 
